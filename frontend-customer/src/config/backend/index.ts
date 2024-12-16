b835a446ebd31806167e6e6ad5e91b257ac0c05e
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2024-12-16 21:36:08 +0700

    update api backend url

--- frontend-customer/src/config/backend/index.ts
+++ frontend-customer/src/config/backend/index.ts
@@ -1,2 +1,2 @@
-export const API_BACKEND_URL = 'http://localhost:2024';
-// export const API_BACKEND_URL = 'https://anhnamcham.onrender.com/';
+// export const API_BACKEND_URL = 'http://localhost:2024';
+export const API_BACKEND_URL = 'https://anhnamcham.onrender.com/';

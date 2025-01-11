3df00d35949afa7f847c567f81438039c11e5b97
Duong Minh Nhat <nhatdmse173465@fpt.edu.vn>
2025-01-11 16:04:50 +0700

    update validate put prudctcard

--- backend-backup/src/routes/productCard/index.ts
+++ backend-backup/src/routes/productCard/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import productCardController from '../../controllers/productCard.controller';
 import { apiKey,permission } from '../../auth/checkAuth';
 import { authentication } from '../../auth/authUtils';
+import { validateProductCard } from '../../middleware/validateProductCard.middleware';
 
 const productCardRouter = Router();
 
@@ -15,8 +16,8 @@ productCardRouter.use(apiKey)
 productCardRouter.use(permission('admin'));
 ////////////////////////////
 
-productCardRouter.post('/', productCardController.createProductCard);
-productCardRouter.put('/:id', productCardController.updateProductCard);
+productCardRouter.post('/', validateProductCard, productCardController.createProductCard);
+productCardRouter.put('/:id', validateProductCard, productCardController.updateProductCard);
 
 
 export default productCardRouter;
